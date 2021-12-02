d54ba6b28b022a9657e7838d9326c062c42910f6
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2021-12-02 15:13:03 +0100

    Added color output

--- src/retval.cpp
+++ src/retval.cpp
@@ -1,7 +1,7 @@
 //: Title       : retval
 //: Date        : 2021-07-10
 //: Author      : Kjetil Kristoffer Solberg <post@ikjetil.no>
-//: Version     : 1.0
+//: Version     : 1.2
 //: Description : Application starts and immediately exits with exit code given as argument.
 //: Usage       : retval <integer exit code>
 //
@@ -13,15 +13,22 @@
 //
 // using
 //
-using std::atoi;
+using std::stoi;
 using std::cout;
 using std::endl;
+
+//
+// #define
+//
+#define VERSION_NO     "1.2"
+#define CLR_GREEN      "\033[32m"
+#define CLR_WHITE      "\033[37;1m"
+#define CLR_RESET      "\033[0m"
 
 //
 // Function Prototypes
 //
 void PrintUsage();
-bool IsArgNumber(char *num);
 
 //
 // Function: main
@@ -34,13 +41,20 @@ int main(int argc, char *argv[])
 		PrintUsage();
 		return EXIT_FAILURE;
 	}
-
-	if ( !IsArgNumber(argv[1]) ) {
+	
+	int r = 0;
+	try
+	{
+		r = stoi(argv[1]);
+	}
+	catch ( std::logic_error ) {
 		PrintUsage();
+		cout << "ABEND ERROR: INVALID ARGUMENT VALUE. NOT A NUMBER." << endl;
 		return EXIT_FAILURE;
 	}
 
-	return atoi(argv[1]);
+	cout << "Return value is: " << r << endl;
+	return r;
 }
 
 //
@@ -50,35 +64,17 @@ int main(int argc, char *argv[])
 //
 void PrintUsage()
 {
-	cout << "retval v1.0 (amd64)" << endl;
-	cout << "Usage: retval <return value integer>" << endl;
-	cout << endl;
-	cout << "Example: (return with exit/return code 5)" << endl;
-	cout << "? retval 5" << endl;
-	cout << "? echo $?" << endl;
-	cout << "5" << endl;
-	cout << endl;
-}
-
-//
-// Function: IsArgNumber
-//
-// (i): Is given string argument a number.
-//
-bool IsArgNumber(char *num)
-{
-	if (num == nullptr) {
-		return false;
-	}
-
-	for (int i = 0; i < strlen(num); i++) {
-		if (i == 0 && (num[i] == '+' || num[i] == '-')) {
-			continue;
-		}
-		if (!isdigit(num[i])) {
-			return false;
-		}
-	}
-
-	return true;
+	cout << CLR_RESET << CLR_GREEN;
+	cout << "##" << endl;
+	cout << "## Return Value" << endl;
+	cout << "## Author  : Kjetil Kristoffer Solberg <post@ikjetil.no>" << endl;
+	cout << "## Version : " << VERSION_NO << endl;
+	cout << "## Usage   : retval <return value integer>" << endl;
+	cout << "##" << endl;
+	cout << "## Example: (return with exit/return code 5)" << endl;
+	cout << "## ? retval 5" << endl;
+	cout << "## ? echo $?" << endl;
+	cout << "## 5" << endl;
+	cout << "##" << endl;
+	cout << CLR_RESET << CLR_WHITE;
 }
