fb5239d89ad46ddcf08e709ab4e966ba0ce828df
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-08-31 15:11:08 +0200

    Moved code into namespace. Redirected main.

--- src/retval.cpp
+++ src/retval.cpp
@@ -10,66 +10,80 @@
 #include <iostream>
 #include <cstring>
 
-//
-// using
-//
-using std::stoi;
-using std::cout;
-using std::endl;
+namespace ItSoftware::RetVal
+{
+	//
+	// using
+	//
+	using std::stoi;
+	using std::cout;
+	using std::endl;
 
-//
-// #define
-//
-#define VERSION_NO     "1.2"
-#define CLR_GREEN      "\033[32m"
-#define CLR_WHITE      "\033[37;1m"
-#define CLR_RESET      "\033[0m"
+	//
+	// #define
+	//
+	#define VERSION_NO     "1.2"
+	#define CLR_GREEN      "\033[32m"
+	#define CLR_WHITE      "\033[37;1m"
+	#define CLR_RESET      "\033[0m"
 
-//
-// Function Prototypes
-//
-void PrintUsage();
+	//
+	// Function Prototypes
+	//
+	void PrintUsage();
+
+	//
+	// Function: main
+	//
+	// (i): Application entry point.
+	//
+	int main(int argc, char *argv[])
+	{
+		if (argc != 2) {
+			PrintUsage();	
+			cout << "ABEND FAILURE. INVALID NUMBER OF ARGUMENTS." << endl;
+			return EXIT_FAILURE;
+		}
+		
+		int r = 0;
+		try
+		{
+			r = stoi(argv[1]);
+		}
+		catch ( std::logic_error ) {
+			PrintUsage();
+			cout << "ABEND FAILURE: INVALID ARGUMENT VALUE. NOT A NUMBER." << endl;
+			return EXIT_FAILURE;
+		}
+
+		return r;
+	}
+
+	//
+	// Function: PrintUsage
+	//
+	// (i): Prints usage screen.
+	//
+	void PrintUsage()
+	{
+		cout << CLR_RESET << CLR_GREEN;
+		cout << "##" << endl;
+		cout << "## Return Value" << endl;
+		cout << "## Author  : Kjetil Kristoffer Solberg <post@ikjetil.no>" << endl;
+		cout << "## Version : " << VERSION_NO << endl;
+		cout << "## Usage   : retval <return value integer>" << endl;
+		cout << "##" << endl;
+		cout << CLR_RESET << CLR_WHITE;
+	}
+}
 
 //
 // Function: main
 //
 // (i): Application entry point.
+//      Redirects to ItSoftware::RetVal::main.
 //
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	if (argc != 2) {
-		PrintUsage();	
-		cout << "ABEND FAILURE. INVALID NUMBER OF ARGUMENTS." << endl;
-		return EXIT_FAILURE;
-	}
-	
-	int r = 0;
-	try
-	{
-		r = stoi(argv[1]);
-	}
-	catch ( std::logic_error ) {
-		PrintUsage();
-		cout << "ABEND FAILURE: INVALID ARGUMENT VALUE. NOT A NUMBER." << endl;
-		return EXIT_FAILURE;
-	}
-
-	return r;
-}
-
-//
-// Function: PrintUsage
-//
-// (i): Prints usage screen.
-//
-void PrintUsage()
-{
-	cout << CLR_RESET << CLR_GREEN;
-	cout << "##" << endl;
-	cout << "## Return Value" << endl;
-	cout << "## Author  : Kjetil Kristoffer Solberg <post@ikjetil.no>" << endl;
-	cout << "## Version : " << VERSION_NO << endl;
-	cout << "## Usage   : retval <return value integer>" << endl;
-	cout << "##" << endl;
-	cout << CLR_RESET << CLR_WHITE;
+	return ItSoftware::RetVal::main(argc, argv);
 }
